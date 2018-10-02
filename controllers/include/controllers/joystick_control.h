e8dd8183cac5c75b1e9f97c1ee538fc4f556ee5d
Greg Droge <greg.droge@usu.edu>
2018-10-02 16:27:23 -0600

    Add a simple joystick controller

---
+++ controllers/include/controllers/joystick_control.h
@@ -0,0 +1,34 @@
+#ifndef __JOYSTICK_CONTROL__
+#define __JOYSTICK_CONTROL__
+
+#include "ros/ros.h"
+#include "sensor_msgs/Joy.h"
+
+#include "geometry_msgs/Twist.h"
+
+namespace controllers
+{
+
+class JoystickControl
+{
+public:
+    JoystickControl();
+
+public:
+
+    ros::NodeHandle n;
+    ros::Publisher pub_command;
+
+    geometry_msgs::Twist cmd;
+
+    void printMessage();
+
+    void joystickCallback(const sensor_msgs::Joy::ConstPtr & msg);
+
+    void publishCommand();
+
+
+};
+
+}
+#endif
